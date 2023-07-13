007b9928a2b4f078c0371295509955565997e496
ConquerorCletus <smartchibuike2019@gmail.com>
2023-07-13 10:30:16 +0000

    Base_model class created

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,4 @@
-from datetime import now
+from datetime import datetime
 from uuid import uuid4
 
 """
@@ -14,3 +14,13 @@ methods:
     1.save: updates the public instance attribute updated_at with the current datetime.
     2.to_dict: returns a dictionary containing all keys/values of __dict__ of the instance.
 """
+class BaseModel():
+
+
+    def __init__(self):
+        self.id = uuid4()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return "[<class name>] (<self.id>) <self.__dict__>".format(name,id,self.__dict__)
