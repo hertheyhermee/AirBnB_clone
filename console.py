cb99bb8e321bf62e2ba16b238ee8e2b7c37c0844
hertheyhermee <tiamiyuhayzed@gmail.com>
2023-07-15 03:30:56 -0700

    console.py

---
+++ console.py
@@ -0,0 +1,28 @@
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Defined the HolbertonBnB command interpreter.
+
+    Attributes:
+        prompt: The command prompt.
+        """
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF signal to exit the program."""
+        print("")
+        return True
+
+    def emptyline(self):
+        """Do nothing upon receiving an empty line."""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
