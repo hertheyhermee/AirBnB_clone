7a9e0a936426a303d4cfde15b48f25c1f6bd76bb
ConquerorCletus <smartchibuike2019@gmail.com>
2023-07-16 21:07:28 +0000

    user and state in console.py

--- console.py
+++ console.py
@@ -8,11 +8,11 @@ import shlex
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
-# from models.place import Place
+from models.place import Place
 from models.state import State
-# from models.city import City
+from models.city import City
 # from models.amenity import Amenity
-# from models.review import Review
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
