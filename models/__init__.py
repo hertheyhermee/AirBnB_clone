a2255f4d80527b7ca45f7a4256434b6948d098cb
ConquerorCletus <smartchibuike2019@gmail.com>
2023-07-16 23:34:47 +0000

    __init__.py in models and state rename

--- models/__init__.py
+++ models/__init__.py
@@ -5,14 +5,20 @@ Package: models
 
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 classes = {"BaseModel": BaseModel,
-           # "User": User,
-           # "Place": Place,
-           # "State": State,
-           # "City": City,
-           # "Amenity":Amenity,
-           # "Review": Review
+           "User": User,
+           "Place": Place,
+           "State": State,
+           "City": City,
+           "Amenity":Amenity,
+           "Review": Review
            }
 storage = FileStorage()
 storage.reload()
