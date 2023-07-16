404deb12539a5a3d2c981dc14c808441e1de484f
hertheyhermee <tiamiyuhayzed@gmail.com>
2023-07-16 15:42:52 -0700

    test city model

---
+++ tests/test_models/test_city_model.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+'''
+    Description:
+        All the test for the user model are implemented here.
+'''
+
+import unittest
+from models.base_model import BaseModel
+from models.city import City
+
+
+class TestUser(unittest.TestCase):
+    '''
+        Description:
+            Testing User class
+    '''
+
+    def test_City_inheritance(self):
+        '''
+            Description:
+                tests that the City class Inherits from BaseModel
+        '''
+        new_city = City()
+        self.assertIsInstance(new_city, BaseModel)
+
+    def test_User_attributes(self):
+        new_city = City()
+        self.assertTrue("state_id" in new_city.__dir__())
+        self.assertTrue("name" in new_city.__dir__())
+
+    def test_type_name(self):
+        '''
+            Description:
+                Test the type of name
+        '''
+        new_city = City()
+        name = getattr(new_city, "name")
+        self.assertIsInstance(name, str)
+
+    def test_type_name(self):
+        '''
+            Description:
+                Test the type of name
+        '''
+        new_city = City()
+        name = getattr(new_city, "state_id")
+        self.assertIsInstance(name, str)
