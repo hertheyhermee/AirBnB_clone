8bb6a844cce08b3490aaf5b3e9590a350cba78b5
ConquerorCletus <smartchibuike2019@gmail.com>
2023-07-16 21:05:38 +0000

    user and state in console.py

--- console.py
+++ console.py
@@ -7,9 +7,9 @@ import json
 import shlex
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
-# from models.user import User
+from models.user import User
 # from models.place import Place
-# from models.state import State
+from models.state import State
 # from models.city import City
 # from models.amenity import Amenity
 # from models.review import Review
